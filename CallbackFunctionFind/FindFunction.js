3a3971b1a6a7c33bfcec45993145b1956b83e05f
ChucNguyen <nguyenxuanchuc111@gmail.com>
2025-08-17 23:13:48 +0700

    CallbackFunction Find Test

---
+++ CallbackFunctionFind/FindFunction.js
@@ -0,0 +1,14 @@
+Array.prototype.myFind = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      return this[i]; 
+    }
+  }
+  return undefined; 
+};
+
+
+const numbers = [1, 3, 5, 8, 10];
+
+let firstEven = numbers.myFind(c => c % 2 === 0);
+console.log(firstEven); 
